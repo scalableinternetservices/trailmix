6d963690f3eb93830a36d35886ee1bcb679fe3cd
shikham-8 <shikhamody1@gmail.com>
2020-10-16 12:33:19 -0700

    Name changes

--- web/src/view/nav/NavBar.tsx
+++ web/src/view/nav/NavBar.tsx
@@ -12,7 +12,7 @@ import { link } from './Link'
 import { getLoginPath, getPath, getSurveyPath, Route } from './route'
 
 const title = {
-  name: 'CS188',
+  name: 'trailmix',
   path: getPath(Route.HOME),
   title: true,
 }
--- web/src/view/page/HomePage.tsx
+++ web/src/view/page/HomePage.tsx
@@ -9,26 +9,23 @@ import { Link } from '../nav/Link'
 import { AppRouteParams, getPath, Route } from '../nav/route'
 import { Page } from './Page'
 
-interface HomePageProps extends RouteComponentProps, AppRouteParams { }
+interface HomePageProps extends RouteComponentProps, AppRouteParams {}
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function HomePage(props: HomePageProps) {
   return (
     <Page>
       <Hero>
-        <H1>CS 188</H1>
-        <H3>Scalable Internet Services</H3>
+        <H1>Trailmix</H1>
+        <H3>Scalable Internet Services Project</H3>
         <H3>UCLA, Fall 2020</H3>
       </Hero>
       <Content>
         <LContent>
           <Section>
-            <H2>About CS 188</H2>
+            <H2>About Trailmix</H2>
             <Spacer $h4 />
-            <BodyText>
-              ☝️ This course explores advanced topics in highly scalable internet services and their underlying
-              architecture.
-            </BodyText>
+            <BodyText>A hiking application to make finding hikes easier and fun!</BodyText>
             <Spacer $h4 />
             <BodyText>
               Software today is increasingly delivered as a service: accessible globally via web browsers and mobile
