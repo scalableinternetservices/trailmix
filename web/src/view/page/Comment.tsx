3d3390bcd15c38a1240a69e9fb4bd4b9ed90810c
Armaan Singh <armaan.singh@gmail.com>
2020-10-20 21:20:48 -0700

    made comment look good

--- web/src/view/page/Comment.tsx
+++ web/src/view/page/Comment.tsx
@@ -6,16 +6,22 @@ interface commentInfo {
   time?: string
 }
 
+const buttonStyle: commentStyle = {
+  outline: 'none',
+  width: '40%',
+  borderRadius: '25px',
+  opacity: 1,
+}
+
 export function CommentCard(args: commentInfo) {
   return (
-    <div className="media mb-3">
-      <img
+    <div className="d-flex aligh-items-start pa3 bg-light-blue" style={buttonStyle}>
+      <div className="mr-3 bg-light rounded">{args.name}</div>
+      {/* <img
         className="mr-3 bg-light rounded"
-        width="48"
-        height="48"
         // src={} //TODO: use dog api to get user photos
         alt={args.name}
-      />
+      /> */}
 
       <div className="media-body p-2 shadow-sm rounded bg-light border">
         <small className="float-right text-muted">{args.time}</small>
