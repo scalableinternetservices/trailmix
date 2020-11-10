a0bac28480886dd5fdb3a2a73bc1052847c5a21c
Armaan Singh <armaan.singh@gmail.com>
2020-11-10 14:52:42 -0800

    comments section in hikelist

--- web/src/view/page/HikeList.tsx
+++ web/src/view/page/HikeList.tsx
@@ -7,6 +7,7 @@ import * as React from 'react'
 import { Component } from 'react'
 import { TrailDesc, TrailTitle } from '../../style/header'
 import { AppRouteParams } from '../nav/route'
+import { CommentsSection } from './CommentSection'
 
 //const TD = style('td', 'mid-gray pa3 v-mid', { minWidth: '7em' })
 
@@ -27,6 +28,9 @@ export interface Trail {
   conditionDate: string
   lat: number
   lon: number
+  comments: string[]
+  names: string[]
+  dates: string[]
 }
 
 interface trailInfo {
@@ -41,6 +45,9 @@ interface trailInfo {
   conditionDate?: string
   icon?: undefined | string
   onClick?: () => void | undefined
+  comments: string[]
+  names: string[]
+  dates: string[]
 }
 
 interface trailStyle {
@@ -130,7 +137,12 @@ export default class HikeList extends Component<HikingListProps, { open: boolean
           <TrailTitle className="pv2">{args.title}</TrailTitle>
           <TrailDesc className="pb2">{args.summary} </TrailDesc>
         </div>
-        <Dialog onClose={args.onClick} aria-labelledby="customized-dialog-title" open={!!this.openTabs.get(args.title)}>
+        <Dialog
+          // disableBackdropClick={true}
+          onClose={args.onClick}
+          aria-labelledby="customized-dialog-title"
+          open={!!this.openTabs.get(args.title)}
+        >
           <DialogTitle id="customized-dialog-title" onClose={() => this.togglePopup(args.title, 'close')}>
             {args.title}
           </DialogTitle>
@@ -140,6 +152,7 @@ export default class HikeList extends Component<HikingListProps, { open: boolean
               This {args.length}-mile hike, located in {args.location}, has {args.difficulty}-level difficulty and is
               currently rated {args.stars} stars.
             </Typography>
+            <CommentsSection comments={args.comments} names={args.names} dates={args.dates} />
           </DialogContent>
         </Dialog>
       </div>
@@ -158,6 +171,9 @@ export default class HikeList extends Component<HikingListProps, { open: boolean
         conditionStatus: item.conditionStatus,
         conditionDetails: item.conditionDetails,
         conditionDate: item.conditionDate,
+        comments: item.comments,
+        names: item.names,
+        dates: item.dates,
       })
     })
   }
