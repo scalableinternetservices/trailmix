af1b344c189c7db151236486de0718a1b1036682
Jivan Gubbi <Jivan@Jivans-MBP-2.attlocal.net>
2020-11-09 18:16:19 -0800

    Fixed upvote/downvote front end

--- web/src/view/page/Comment.tsx
+++ web/src/view/page/Comment.tsx
@@ -26,13 +26,31 @@ const buttonStyle: commentStyle = {
   borderRadius: '25px',
   opacity: 1,
 }
-async function like(obj: UpvoteInput) {
+async function like(obj: UpvoteInput, changeUp: any, changeDown: any, upvoted: boolean, downvoted: boolean) {
+  if (upvoted) {
+    return
+  }
+  if (downvoted) {
+    await upvote(getApolloClient(), obj)
+  }
+  changeUp(true)
+  changeDown(false)
   await upvote(getApolloClient(), obj)
 }
-async function unlike(obj: DownvoteInput) {
+async function unlike(obj: DownvoteInput, changeUp: any, changeDown: any, upvoted: boolean, downvoted: boolean) {
+  if (downvoted) {
+    return
+  }
+  if (upvoted) {
+    await downvote(getApolloClient(), obj)
+  }
+  changeUp(false)
+  changeDown(true)
   await downvote(getApolloClient(), obj)
 }
 export function CommentCard(props: commentInfo) {
+  const [upvoted, changeUp] = React.useState(false)
+  const [downvoted, changeDown] = React.useState(false)
   return (
     <div className="d-flex align-items-start pa3 bg-light-blue" style={buttonStyle}>
       <div className="mr-3 bg-light rounded">{props.name}</div>
@@ -48,14 +66,32 @@ export function CommentCard(props: commentInfo) {
         {props.message}
         <Spacer></Spacer>
         <Checkbox
-          onClick={() => like({ name: props.name, text: props.message, date: props.time, id: 1 })}
+          onClick={() =>
+            like(
+              { name: props.name, text: props.message, date: props.time, id: 1 },
+              changeUp,
+              changeDown,
+              upvoted,
+              downvoted
+            )
+          }
           icon={<ThumbUpIcon />}
+          checked={upvoted}
           checkedIcon={<ThumbUpIcon />}
           name="checkedH"
         />
         <Checkbox
-          onClick={() => unlike({ name: props.name, text: props.message, date: props.time, id: 1 })}
+          onClick={() =>
+            unlike(
+              { name: props.name, text: props.message, date: props.time, id: 1 },
+              changeUp,
+              changeDown,
+              upvoted,
+              downvoted
+            )
+          }
           icon={<ThumbDownIcon />}
+          checked={downvoted}
           checkedIcon={<ThumbDownIcon />}
           name="checkedH"
         />
