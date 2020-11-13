16d842682ab39273fdb5465fb1f84b64d71c9e07
shikham-8 <shikhamody1@gmail.com>
2020-11-13 15:29:07 -0800

    Load testing experiments

--- server/src/loadtest/kbScript.js
+++ server/src/loadtest/kbScript.js
@@ -1,6 +1,3 @@
-import http from 'k6/http'
-import { sleep } from 'k6'
-
 //see this link for k6 information: https://k6.io/docs/getting-started/running-k6
 //make sure server is running before running this load test
 //k6 will output information about its test, and data can also be visualized
@@ -9,14 +6,68 @@ import { sleep } from 'k6'
 //to install k6 on Mac, use "brew install k6" in terminal
 //to run this script, use the command: k6 run server/src/loadtest/kbScript.js
 
-export let options = {
-  vus: 10, //number of virtual users
-  duration: '30s', //duration of load test
+import http from 'k6/http'
+import { sleep } from 'k6'
+import { Counter, Rate } from 'k6/metrics'
+
+export const options = {
+  scenarios: {
+    example_scenario: {
+      // name of the executor to use
+      executor: 'ramping-arrival-rate',
+      // common scenario configuration
+      startRate: '50',
+      timeUnit: '1s',
+      // executor-specific configuration
+      preAllocatedVUs: 50,
+      maxVUs: 100,
+      stages: [
+        { target: 200, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+    },
+  },
 }
 
 export default function () {
-  http.get('http://127.0.0.1:3000/app')
-  sleep(1)
-  http.get('http://127.0.0.1:3000/hiking')
-  sleep(1)
+  recordRates('http://localhost:3000')
+  // recordRates(
+  const resp = http.post(
+    'http://localhost:3000/graphql',
+    '{"operationName":"FetchLatLon","variables":{"zipcode":90024},"query":"query FetchLatLon($zipcode: Int!) {\n  coordinates(zipcode: $zipcode) {\n    lat\n    lon\n    __typename\n  }\n}\n"}',
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    }
+  )
+  // )
+  sleep(Math.random() * 3)
+}
+
+const count200 = new Counter('status_code_2xx')
+const count300 = new Counter('status_code_3xx')
+const count400 = new Counter('status_code_4xx')
+const count500 = new Counter('status_code_5xx')
+
+const rate200 = new Rate('rate_status_code_2xx')
+const rate300 = new Rate('rate_status_code_3xx')
+const rate400 = new Rate('rate_status_code_4xx')
+const rate500 = new Rate('rate_status_code_5xx')
+
+function recordRates(res) {
+  if (res.status >= 200 && res.status < 300) {
+    count200.add(1)
+    rate200.add(1)
+  } else if (res.status >= 300 && res.status < 400) {
+    console.log(res.body)
+    count300.add(1)
+    rate300.add(1)
+  } else if (res.status >= 400 && res.status < 500) {
+    count400.add(1)
+    rate400.add(1)
+  } else if (res.status >= 500 && res.status < 600) {
+    count500.add(1)
+    rate500.add(1)
+  }
 }
